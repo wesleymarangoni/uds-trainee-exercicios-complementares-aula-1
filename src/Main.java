a3d38bb0f0a9adfd8681d5347d101c9a0a0292ff
wesley <wesleymarangoni@hotmail.com>
2018-06-14 16:31:38 -0300

    Atividade 1, 2 e 3 Resolvido

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
+        int numero;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Verifica Se Numero é Par ou Impar");
+        System.out.println("Digite um numero: ");
+        numero = entrada.nextInt();
+        System.out.println(verificaNumeroParouImpar(numero));
+    }
+    public static String verificaNumeroParouImpar(int numero){
+        String ResultadoParOuImpar;
+        if(numero % 2 == 0)
+            ResultadoParOuImpar ="O numero é par";
+        else
+            ResultadoParOuImpar = "O numero é impar";
+        return(ResultadoParOuImpar);
     }
 }
